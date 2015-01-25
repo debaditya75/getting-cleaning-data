78b12ff3cdb1a1db6bb74d82066a2978a9d41e28
debaditya75 <debaditya.chatterjee@gmail.com>
2015-01-24 23:35:40 -0800

    using aggregate function

--- run_analysis.R
+++ run_analysis.R
@@ -10,14 +10,16 @@
 # Inside this folder both the test and the training set files are available.
 
 # load the packages required by the program
-# the melt function will be used here
-library("reshape2") 
 #the data.table is used 
 library("data.table")
 # for the merge function
 library("dplyr")
 
 # 
+# Load and process test_x & test_y data.
+test_x <- read.table("./UCI HAR Dataset/test/X_test.txt")
+test_y <- read.table("./UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 
 # Load the feature names
 features <- read.table("./UCI HAR Dataset/features.txt")[,2]
@@ -27,11 +29,6 @@ activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
 # Extract the measurement names on the mean and standard deviation
 extract_features <- grepl("mean|std", features)
-
-# Load and process test_x & test_y data.
-test_x <- read.table("./UCI HAR Dataset/test/X_test.txt")
-test_y <- read.table("./UCI HAR Dataset/test/y_test.txt")
-subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 
 # Now assign the features as the column names
 names(test_x) = features
@@ -72,8 +69,6 @@ train_data <- cbind(as.data.table(subject_train), train_y, train_x)
 data = rbind(test_data, train_data)
 
 id_labels   = c("subject", "Activity_ID", "Activity_Label")
-data_labels = setdiff(colnames(data), id_labels)
-melt_data      = melt(data, id = id_labels, measure.vars = data_labels)
-# Apply mean function to dataset using dcast function
-tidy_data   = dcast(melt_data, subject + Activity_Label ~ variable, mean)
-write.table(tidy_data, file = "./tidy_data.txt")
+tidy_data   = suppressWarnings(aggregate(data, by=list(Activity_Labels= data$Activity_Label, subjects=data$subject),FUN=mean))
+tidy_data1 <- tidy_data[,!(names(tidy_data) %in% id_labels)]
+write.table(tidy_data1, file = "./tidy_data.txt")
